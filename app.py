f416d8a5c8a5d45aba436cbfebe124b8d53e8f0c
directionvaikeashop-cloud <directionvaikeashop@gmail.com>
2026-07-09 19:48:20 -1000

    Update app.py

--- app.py
+++ app.py
@@ -35,6 +35,7 @@ from generators import rui
 from generators import tureia
 from generators import champagne
 from generators import fan90
+from generators import oaoa
 
 app = Flask(__name__)
 app.secret_key = os.environ.get("MANAPRINT_SECRET", "dev-secret-a-changer-en-prod")
@@ -187,6 +188,7 @@ _enregistrer_paire("rui",           "RUI","🎴", 12, rui.generer_pdf)
 _enregistrer_paire("tureia",        "TUREIA","🔶", 6,  tureia.generer_pdf)
 _enregistrer_paire("champagne",     "CHAMPAGNE","🥂", 6,  champagne.generer_pdf)
 _enregistrer_paire("fan90",         "FAN 90","☀️", 8,  fan90.generer_pdf)
+_enregistrer_paire("oaoa",          "OAOA","⭕", 12, oaoa.generer_pdf)
 # --- Ajouter un futur jeu A4 = UNE ligne _enregistrer_paire(...) (crée Couleur + N&B) ---
 # _enregistrer_paire("ohana90", "OHANA 90", "🌺", 8, ohana90.generer_pdf)
 
@@ -475,6 +477,7 @@ _PLAGES_CALLER = {
     "tureia": (1, 75),
     "champagne": (1, 75),
     "fan90": (1, 90),
+    "oaoa": (16, 75),
 }
 
 
@@ -483,6 +486,7 @@ _BOULES_CALLER = {
     "bno": [n for n in range(1, 16)] + [n for n in range(31, 46)] + [n for n in range(61, 76)],
     "tureia": [n for n in range(1, 31)] + [n for n in range(46, 76)],  # colonne 31-45 morte
     "fan90": [n for n in range(1, 11)] + [n for n in range(20, 91)],   # sans le 11 à 19
+    "oaoa": [n for n in range(16, 31)] + [n for n in range(61, 76)],   # O 16-30 et A 61-75
 }
 
 
