5d0f65c580975df6c813eb0881e7ce1b9beab20c
directionvaikeashop-cloud <directionvaikeashop@gmail.com>
2026-07-21 15:03:07 -1000

    Update app.py

--- app.py
+++ app.py
@@ -27,6 +27,7 @@ from generators import pow as powgen
 from generators import poe_parau as poeparaugen
 from generators import hakari as hakarigen
 from generators import henua_enana as henuaenanagen
+from generators import tiare as tiaregen
 from generators import win
 from generators import rubis90
 from generators import vai
@@ -258,6 +259,7 @@ _enregistrer_paire("pow",           "POW 8 boules","💥", 12, powgen.generer_pd
 _enregistrer_paire("poe_parau",     "POE PARAU 6 boules", "🦪", 12, poeparaugen.generer_pdf)
 _enregistrer_paire("hakari",        "HAKARI 6 boules", "🥥", 12, hakarigen.generer_pdf)
 _enregistrer_paire("henua_enana",   "HENUA ENANA 7 boules", "🗺️", 12, henuaenanagen.generer_pdf)
+_enregistrer_paire("tiare",         "TIARE 50-90", "🌼", 12, tiaregen.generer_pdf)
 _enregistrer_paire("win",           "WIN 9 boules","🏆", 12, win.generer_pdf)
 _enregistrer_paire("rubis90",       "RUBIS 90","💎", 12, rubis90.generer_pdf)
 _enregistrer_paire("vai",           "VAI 9 boules","🌊", 12, vai.generer_pdf)
@@ -757,6 +759,7 @@ _PLAGES_CALLER = {
     "poe_parau": (1, 75),
     "hakari": (1, 75),
     "henua_enana": (1, 75),
+    "tiare": (50, 90),
     "win": (1, 45),
     "rubis90": (1, 90),
     "vai": (61, 90),
