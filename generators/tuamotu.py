310685600905daf10a68e70384cbc18d5a784eb1
directionvaikeashop-cloud <directionvaikeashop@gmail.com>
2026-07-23 19:55:45 -1000

    Update tuamotu.py

--- generators/tuamotu.py
+++ generators/tuamotu.py
@@ -68,7 +68,7 @@ def _charger_tuamotu():
             fondb = _Image.new("RGBA", brut.size, (255, 255, 255, 255))
             brut = _Image.alpha_composite(fondb, brut)
         img = brut.convert("L")
-        img = img.point(lambda p: int(255 - (255 - p) * 0.18))
+        img = img.point(lambda p: int(255 - (255 - p) * 0.45))
         _TUAMOTU_IMG = img.convert("RGB")
     except Exception:
         _TUAMOTU_IMG = False
